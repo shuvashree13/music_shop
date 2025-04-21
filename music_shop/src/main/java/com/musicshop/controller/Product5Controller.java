3563c832124f4853ee6de01d1bac04504247d5b0
Swodhin <83653801+Swodhin@users.noreply.github.com>
2025-04-21 22:31:28 +0545

    Create Product5Controller.java

---
+++ music_shop/src/main/java/com/musicshop/controller/Product5Controller.java
@@ -0,0 +1,20 @@
+package com.islington.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Swodhin Prajapati
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/product5" })
+public class Product5Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/pages/product5.jsp").forward(request, response);
+	}
+}
