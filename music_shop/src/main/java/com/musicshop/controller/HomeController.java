9bacb4bc900b9bfb9355d014c7c4f9928de28775
Swodhin <83653801+Swodhin@users.noreply.github.com>
2025-04-21 17:16:44 +0545

    Update HomeController.java

--- music_shop/src/main/java/com/musicshop/controller/HomeController.java
+++ music_shop/src/main/java/com/musicshop/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.musicshop.controller;
+package com.islington.controller;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,35 +8,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * @author shuva shree adhikari
- * Servlet implementation class Home
+ * @author Swodhin Prajapati
  */
-@WebServlet(asyncSupported = true, urlPatterns = { "/home", "/" })
+@WebServlet(asyncSupported = true, urlPatterns = { "/home", "/"})
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
